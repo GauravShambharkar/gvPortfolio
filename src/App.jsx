5bac535c0327a5f2a86e2feb243d39dff6752efa
Gaurav Shambharkar <shambharkargaurav04@gmail.com>
2025-07-06 12:52:11 +0530

    fiexd layout

--- src/App.jsx
+++ src/App.jsx
@@ -3,117 +3,67 @@ import "./App.css";
 function App() {
   return (
     <>
-      {/* <div className="w-full h-screen bg-[#DBDBDB]  ">
-        <div className="header border w-full px-12 h-fit flex flex-col ">
-          <div className="box flex flex-col items-center text-center mt-5 ">
-            <div className="tracking-tighter text-9xl border w-fit font-extrabold text-[#FF0000] ">
-              MYSELF GAURAV,
-            </div>
-          </div>
-          <div className="w-[55%] border text-left ml-49 text-[#676767] tracking-tighter text-l leading-none">
-            I am a <span className="text-black"> software developer </span>{" "}
-            passionate about crafting dynamic and user-centric web apps. With
-            experience in modern technologies, I thrive on building scalable and
-            intuitive web solutions. I embrace challenges as opportunities to
-            learn and grow as a developer.
-          </div>
-        </div>
-        <div className="hero  px-12 w-full h-[70%] border">
-          <div className="navBar  w-fit h-fit">
-            <div className="leading-none border fixed top-72 left-12 font-bold text-white  ">
-              <h4 className="text-[#FF0000]">HOME</h4>
-              <h4>ABOUT US</h4>
-              <h4>SKILLS</h4>
-              <h4>RECENT WORK</h4>
-              <h4>RESUME</h4>
-              <h4>EDUCATION</h4>
-            </div>
-            <div className="leading-none border fixed top-72 right-12 font-bold text-white  ">
-              <h4 className="text-[#FF0000]">GIT-HUB</h4>
-              <h4>LINKEDIN</h4>
-              <h4>X WORK</h4>
-            </div>
-          </div>
-        </div>
-
-        <div className="w-full h-screen bg-[#DBDBDB]  ">
-          <div className="tracking-tighter absolute top-163 text-9xl border font-extrabold text-[#FF0000] ">
-            ABOUT US,
-          </div>
-        </div>
-      </div> */}
-      <div className="section w-full h-screen bg-[#CBCBCB] ">
-        {/* //header */}
-        <div className="header w-[95%] border mx-auto  ">
-          <h1 className=" text-center font-extrabold text-9xl text-[#FF0000] tracking-tighter ">
+      <div className="w-[95%] border mx-auto bg-[#CBCBCB]">
+        <div className="headerContainer border ">
+          <h1 className=" text-center text-9xl font-extrabold text-[#FF0000] tracking-tighter ">
             MYSELF GAURAV,
           </h1>
-          <div className="w-full flex justify-center text-left  ">
-            {/* paragraph */}
-            <div className="w-[71%] pl-13">
-              <p className="w-fit leading-none  text-[#676767]">
-                I am a{" "}
-                <span className="text-[#000000] font-light">
-                  FrontEnd Developer
-                </span>{" "}
-                passionate about crafting dynamic and user-centric web apps.{" "}
-                <br /> With experience in modern technologies, I thrive on
-                building scalable and intuitive web solutions. <br /> I embrace
-                challenges as opportunities to learn and grow as a developer.
-              </p>
-            </div>
+          <div className="paraContainer border flex justify-center ">
+            <p className="w-[70%] border leading-none text-[#676767]">
+              I am a{" "}
+              <span className="text-[#000000] font-light">
+                FrontEnd Developer
+              </span>{" "}
+              passionate about crafting dynamic and user-centric web apps.{" "}
+              <br /> With experience in modern technologies, I thrive on
+              building scalable and intuitive web solutions. <br /> I embrace
+              challenges as opportunities to learn and grow as a developer.
+            </p>
           </div>
         </div>
-
-        {/* middle section */}
-
-        <div className="header w-[95%] mt-1 border mx-auto flex">
-          <div className="MenuContainer border flex justify-between items-center w-full h-fit">
-            {/* left nav ELements */}
-            <div className="LeftMenues border tracking-tighter z-10 fixed top-80 leading-none  flex flex-col justify-center font-bold text-white  ">
-              <h4 className="text-[#FF0000] ">HOME</h4>
-              <h4>ABOUT ME</h4>
-              <h4>SKILLS</h4>
-              <h4>RECENT WORK</h4>
-              <h4>RESUME</h4>
-              <h4>EDUCATION</h4>
-            </div>
-            {/* img */}
-            <div className="middle_Img w-90 border flex items-center justify-center mx-auto">
-              <div className="flex justify-center absolute items-center">
-                <div className="blurDot size-120 -z-0 rounded-full blur-3xl text-[#FF0000] absolute bg-[#FF0000]/70">
-                  .
-                </div>
-              </div>
-
+        <div className="navContainer flex border justify-between items-center border-red-600">
+          <div className="leftSide w-fit fixed h-fit z-10 top-80 leading-none text-left text-white tracking-tighter font-bold border ">
+            <h4 className="text-red-500">HOME</h4>
+            <h4>ABOUT ME</h4>
+            <h4>SKILLS</h4>
+            <h4>RECENT WORK</h4>
+            <h4>RESUME</h4>
+            <h4>EDUCATION</h4>
+          </div>
+          <div className="center flex w-full justify-center">
+            <div className="middle border z-10 w-90">
               <img
-                className="w-89 object-fill z-10 "
+                className="shadow-2xs grayscale"
                 src="./src/ps-bg-removed.png"
                 alt=""
               />
             </div>
-
-            {/* right nav ELements */}
-            <div className="RightMenues border font-bold fixed top-85 z-10 right-9 text-white leading-none  flex flex-col justify-center text-right">
-              <h4 className="text-[#FF0000]">GIT-HUB</h4>
-              <h4>X</h4>
-              <h4>LINKEDIN</h4>
+            <div className="blurDot size-120 -z-0 rounded-full blur-3xl text-[#FF0000] absolute bg-black/30">
+              .
             </div>
+          </div>
+
+          <div className="rightSide w-fit fixed right-9 z-10 h-fit top-85 leading-none text-right text-white tracking-tighter font-bold border">
+            <h4>GIT-HUB</h4>
+            <h4>LINKEDIN</h4>
+            <h4>X</h4>
+            <h4>EMAIL</h4>
           </div>
         </div>
       </div>
 
-      {/* about section */}
-      <div className="w-[95%] mx-auto border h-screen ">
-        <h1 className="text-9xl border font-bold absolute top-185 text-[#FF0000] tracking-tighter">
-          ABOUT
+      <div className="w-[95%] h-screen border flex items-center mx-auto">
+        <h1 className="text-left border text-9xl font-extrabold absolute top-165 text-[#FF0000] tracking-tighter ">
+          ABOUT ME,
         </h1>
+        <div className="border w-[97%] mx-auto h-[95%]  ">
+          <h2>hellow this is an about page</h2>
+        </div>
       </div>
 
-      {/* skills section */}
-      <div className="w-[95%] mx-auto border h-screen ">
-        <h1 className="text-9xl border font-bold absolute top-370 right-9 text-[#FF0000] tracking-tighter">
-          SKILLS
+      <div className="w-[95%] h-screen border mx-auto">
+        <h1 className="text-right border text-9xl font-extrabold absolute right-9 top-330 text-[#FF0000] tracking-tighter ">
+          SKILLS,
         </h1>
       </div>
     </>
